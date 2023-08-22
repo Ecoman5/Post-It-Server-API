2bc6f914bc971e6f6e10df373879860d32be5bf6
Olatunbosun Solomon <olatunbosunsolomon5@outlook.com>
2023-08-22 21:36:38 +0100

    change caps to lowercase

--- models/story.js
+++ models/story.js
@@ -1,32 +1,31 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const storySchema = new mongoose.Schema({
- title: {
-  type: String,
-  required: [true, 'Story Title is Required'],
- },
- description: {
-  type: String,
-  required: [true, 'Story Description is Required']
- },
- tag: {
-  type: String,
-  enum: ['nature', 'lifestyle', 'sport', 'technology', 'others'],
-  required: [true, 'Story tag is Required']
- },
- Image: {
-  type: String,
- },
- createdBy: {
-  type: mongoose.Types.ObjectId,
-  ref: "User",
-  required: [true, "Please provide a user"],
- },
- createdAt: {
-  type: Date,
-  default: Date.now(),
- },
-},
-);
+  title: {
+    type: String,
+    required: [true, "Story Title is Required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Story Description is Required"],
+  },
+  tag: {
+    type: String,
+    enum: ["nature", "lifestyle", "sport", "technology", "others"],
+    required: [true, "Story tag is Required"],
+  },
+  image: {
+    type: String,
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a user"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 module.exports = mongoose.model("Story", storySchema);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,6 @@
-const router = require('express').Router();
 const { registerUser, loginUser, foundUser } = require('../controllers/authController')
+
+const router = require('express').Router();
 const auth = require("../middleware/auth");
 
 router.post("/register", registerUser)
